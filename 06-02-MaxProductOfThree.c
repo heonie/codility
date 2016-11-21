38b5d72d222b00c0167a27a5155df85aedf326b0
Kookheon Kim <heonie@gmail.com>
2016-11-21 12:32:35 +0900

    Update 06-02-MaxProductOfThree.c

--- 06-02-MaxProductOfThree.c
+++ 06-02-MaxProductOfThree.c
@@ -1,15 +1,15 @@
 int compare(const void *a, const void *b) {
-    if(*(int*)a < *(int*)b) {
-        return -1;
-    }
-    else if(*(int*)a > *(int*)b) {
-        return 1;
-    }
-    else {
-        return 0;
-    }
+    if(*(int*)a < *(int*)b) return -1;
+    if(*(int*)a > *(int*)b) return 1;
+    return 0;
 }
+
 int solution(int A[], int N) {
     qsort(A, N, sizeof(int), compare);
-    return A[N-1] * A[N-2] * A[N-3];
+    if(A[0] * A[1] * A[N-1] > A[N-3] * A[N-2] * A[N-1]) {
+        return A[0] * A[1] * A[N-1];
+    }
+    else {
+        return A[N-3] * A[N-2] * A[N-1];
+    }
 }
